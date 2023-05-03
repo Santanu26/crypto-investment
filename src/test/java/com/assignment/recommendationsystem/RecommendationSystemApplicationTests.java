d6112d3a995c611e2291a26106f20b0063254c49
Santanu Barua <santanu>
2023-05-03 14:33:18 +0200

    Initial commit

---
+++ src/test/java/com/assignment/recommendationsystem/RecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.recommendationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
